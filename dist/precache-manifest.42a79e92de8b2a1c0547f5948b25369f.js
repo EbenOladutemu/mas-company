2b19ca993af624711bbe92bf8971e5e9184b23b6
Ebenezer Oladutemu <eoladutemu@parkwayprojects.com>
2021-09-24 12:24:54 +0100

    Update git ignore. New build

---
+++ dist/precache-manifest.42a79e92de8b2a1c0547f5948b25369f.js
@@ -0,0 +1,262 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a02faf7ea2a9584134ffe15779a0e44",
+    "url": "/_redirects"
+  },
+  {
+    "revision": "fc98fccebac2d0b93b9d",
+    "url": "/css/about.afc14c94.css"
+  },
+  {
+    "revision": "ed192f808bfb5d91e870",
+    "url": "/css/app.026b7481.css"
+  },
+  {
+    "revision": "4fc11b03c47dbfc8a23a",
+    "url": "/css/chunk-4295f931.d44c6908.css"
+  },
+  {
+    "revision": "07c39a4785970c1c3a09",
+    "url": "/css/chunk-4cfb453b.7f10667c.css"
+  },
+  {
+    "revision": "ee8dec99be9e7be611e3",
+    "url": "/css/chunk-6049d463.8d36b80c.css"
+  },
+  {
+    "revision": "cfb22f2d65c7cef4e713",
+    "url": "/css/chunk-vendors.07bc7018.css"
+  },
+  {
+    "revision": "f52d98d7f024a493d51b959ff156dbec",
+    "url": "/htaccess"
+  },
+  {
+    "revision": "b8195f4f7a38d4e8153d6bca8e20d0c3",
+    "url": "/img/Abubakarr-Sesay.b8195f4f.jpeg"
+  },
+  {
+    "revision": "9f519b7d7ed37273d8a84fa004990661",
+    "url": "/img/Dolcie-Hamilton-2.9f519b7d.jpeg"
+  },
+  {
+    "revision": "e4ede66889ab43c23650844d380f7be8",
+    "url": "/img/Dolcie-Hamilton.e4ede668.jpeg"
+  },
+  {
+    "revision": "e484a33c790ab35fef24aca91c86c944",
+    "url": "/img/Henry-Zarkpah-2.e484a33c.jpeg"
+  },
+  {
+    "revision": "85ac7ac07d9667b1681a65c8dcc931d7",
+    "url": "/img/Henry-Zarkpah.85ac7ac0.jpeg"
+  },
+  {
+    "revision": "0babb9a82cd8343f2a3fafbaa309b4ea",
+    "url": "/img/Mamadu-Bah-2.0babb9a8.jpeg"
+  },
+  {
+    "revision": "74580813b55fed05f230cc5fee7dff62",
+    "url": "/img/Mamadu-Bah.74580813.jpeg"
+  },
+  {
+    "revision": "9f5bc9820bd6dff8b322bea1886ed584",
+    "url": "/img/Mohamed-Kamara.9f5bc982.jpeg"
+  },
+  {
+    "revision": "be7e2bd5ca68b9b973a26b2f023cac39",
+    "url": "/img/Sulaiman-Dainkeh-2.be7e2bd5.jpeg"
+  },
+  {
+    "revision": "db15b70f5dd2ffacad93a342e457d6ab",
+    "url": "/img/Sulaiman-Dainkeh.db15b70f.jpeg"
+  },
+  {
+    "revision": "f60c4dc1d7f89f021921caf857955f73",
+    "url": "/img/Sulaiman-Koroma.f60c4dc1.jpeg"
+  },
+  {
+    "revision": "30f39a0be112ebe22f7a9ff6d1b201e5",
+    "url": "/img/about-hero.30f39a0b.jpg"
+  },
+  {
+    "revision": "898b147857693f44cbc102d79d9b0c1d",
+    "url": "/img/hero.898b1478.jpg"
+  },
+  {
+    "revision": "635872346b5fa4d836848b8716a4da0f",
+    "url": "/img/logo.63587234.png"
+  },
+  {
+    "revision": "687407ab4e4b89a49d756eacd5c4d6fd",
+    "url": "/img/project-img-1.687407ab.jpeg"
+  },
+  {
+    "revision": "26fcc72e0ff3e1fee36b51b2446c4769",
+    "url": "/img/project-img-10.26fcc72e.jpeg"
+  },
+  {
+    "revision": "f081197bdd463919f647adef35ce6271",
+    "url": "/img/project-img-11.f081197b.jpeg"
+  },
+  {
+    "revision": "0981bf82c2a8be27619f3f5ebb27a63b",
+    "url": "/img/project-img-12.0981bf82.jpeg"
+  },
+  {
+    "revision": "64ae14147808fadaad432b17fa7ab641",
+    "url": "/img/project-img-13.64ae1414.jpeg"
+  },
+  {
+    "revision": "eb7f296f9e8f25921ef3eb9d4a4c6f56",
+    "url": "/img/project-img-14.eb7f296f.jpeg"
+  },
+  {
+    "revision": "79737682ef674b72b6c8d40a3df2eaef",
+    "url": "/img/project-img-15.79737682.jpeg"
+  },
+  {
+    "revision": "192997aebeba2d6d44448a364a84a170",
+    "url": "/img/project-img-16.192997ae.jpeg"
+  },
+  {
+    "revision": "1c9fae6a5ff4cc4b74a536c84aec7d56",
+    "url": "/img/project-img-17.1c9fae6a.jpeg"
+  },
+  {
+    "revision": "0f2c1880e4491b9d5c2b3ca0cabc9275",
+    "url": "/img/project-img-18.0f2c1880.jpeg"
+  },
+  {
+    "revision": "9f81f8d8d4552916fd3d255cded7998f",
+    "url": "/img/project-img-19.9f81f8d8.jpeg"
+  },
+  {
+    "revision": "5dd009df8b1aa510718e39cbbd03aa27",
+    "url": "/img/project-img-2.5dd009df.jpeg"
+  },
+  {
+    "revision": "8001c78189e3cf2b99813e6a79d93728",
+    "url": "/img/project-img-20.8001c781.jpeg"
+  },
+  {
+    "revision": "e65c5894e394b04f8b82be7072f709e8",
+    "url": "/img/project-img-21.e65c5894.jpeg"
+  },
+  {
+    "revision": "ef790d3f3ddbf915c2b2a5601b6505e9",
+    "url": "/img/project-img-22.ef790d3f.jpeg"
+  },
+  {
+    "revision": "a1054f523427fa82c8fc650d5646c48e",
+    "url": "/img/project-img-23.a1054f52.jpeg"
+  },
+  {
+    "revision": "7f31785e6857059f7ae0e7720e51ed19",
+    "url": "/img/project-img-24.7f31785e.jpeg"
+  },
+  {
+    "revision": "3dd2383e3909d737f79b54ffce016744",
+    "url": "/img/project-img-25.3dd2383e.jpeg"
+  },
+  {
+    "revision": "38eff7eae7d3fdd2cfd93ee51023af68",
+    "url": "/img/project-img-26.38eff7ea.jpeg"
+  },
+  {
+    "revision": "2b3c1b460f782e64702a93fcca72715d",
+    "url": "/img/project-img-27.2b3c1b46.jpeg"
+  },
+  {
+    "revision": "e9a178acdbe0dd3965bcc603e443d62d",
+    "url": "/img/project-img-28.e9a178ac.jpeg"
+  },
+  {
+    "revision": "b3da3943f4461ecbf29c4f74b80424d8",
+    "url": "/img/project-img-29.b3da3943.jpeg"
+  },
+  {
+    "revision": "b8195f4f7a38d4e8153d6bca8e20d0c3",
+    "url": "/img/project-img-29.b8195f4f.jpeg"
+  },
+  {
+    "revision": "cfe32217fa52c047dc7958f18fcf381a",
+    "url": "/img/project-img-3.cfe32217.jpeg"
+  },
+  {
+    "revision": "61305fc58904a36cdbd7c380f1a6ad69",
+    "url": "/img/project-img-30.61305fc5.jpeg"
+  },
+  {
+    "revision": "7a53bc24ec5f75ece1154abb705323b5",
+    "url": "/img/project-img-4.7a53bc24.jpeg"
+  },
+  {
+    "revision": "7a8a4454bea3959b2e5c8168afb4c0cd",
+    "url": "/img/project-img-5.7a8a4454.jpeg"
+  },
+  {
+    "revision": "0dd6a54a03053d85a37e92b929204605",
+    "url": "/img/project-img-6.0dd6a54a.jpeg"
+  },
+  {
+    "revision": "c33573b1e56c05bb41352371eb46d73a",
+    "url": "/img/project-img-7.c33573b1.jpeg"
+  },
+  {
+    "revision": "8268aa8810bbafe490accfcb9a074a68",
+    "url": "/img/project-img-8.8268aa88.jpeg"
+  },
+  {
+    "revision": "2fa6994985cf48d7920b27620f39cf40",
+    "url": "/img/project-img-9.2fa69949.jpeg"
+  },
+  {
+    "revision": "e44350faf6aba536f7229c659c53c3bf",
+    "url": "/img/projects-hero.e44350fa.jpg"
+  },
+  {
+    "revision": "c8c856a4cb2619d1ae987f7fb5e04ade",
+    "url": "/img/services-hero.c8c856a4.jpg"
+  },
+  {
+    "revision": "4e6a9d1b357eaefafdf37e556a6c8d1f",
+    "url": "/img/team-hero.4e6a9d1b.jpg"
+  },
+  {
+    "revision": "13b104ef96cab19346ff5800e85639fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc98fccebac2d0b93b9d",
+    "url": "/js/about.0f249b16.js"
+  },
+  {
+    "revision": "ed192f808bfb5d91e870",
+    "url": "/js/app.c65e2b1d.js"
+  },
+  {
+    "revision": "4fc11b03c47dbfc8a23a",
+    "url": "/js/chunk-4295f931.83036e12.js"
+  },
+  {
+    "revision": "07c39a4785970c1c3a09",
+    "url": "/js/chunk-4cfb453b.9d8620d6.js"
+  },
+  {
+    "revision": "ee8dec99be9e7be611e3",
+    "url": "/js/chunk-6049d463.0d40cf4d.js"
+  },
+  {
+    "revision": "cfb22f2d65c7cef4e713",
+    "url": "/js/chunk-vendors.cefeedae.js"
+  },
+  {
+    "revision": "ca37e9486f584aca4f7a910c53ce3e8d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
